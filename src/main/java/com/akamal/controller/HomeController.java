c4bc3e3c0b2f62671fe06ef80774731656fb845e
aminkamaldev <amin.kamal.dev@gmail.com>
2018-09-28 23:17:17 +0100

    change test

--- src/main/java/com/akamal/controller/HomeController.java
+++ src/main/java/com/akamal/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home() throws UnknownHostException {
 		return "Bienvenue sur le microservice : " + InetAddress.getLocalHost().getHostAddress();
 	}
